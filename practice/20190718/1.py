2db48b191fb459d80a5e5374a90ba323052e9db8
Hwangjb17 <qlghd98@gmail.com>
2019-08-20 20:44:14 +0900

    20190718

---
+++ practice/20190718/1.py
@@ -0,0 +1,38 @@
+# a = "Life is too short, you need python"
+#
+# if "wife" in a: print("wife")
+# elif "python" in a and "you" not in a: print("python")
+# elif "shirt" not in a: print("shirt")
+# elif "need" in a: print("need")
+# else: print("none")
+
+# num=3
+# sum=0
+# while num<1000:
+#     sum+=num
+#     num+=3
+#
+# print(sum)
+
+# i=0
+# j=0
+#
+# while i<5:
+#     j=0
+#     while j<i+1:
+#         print('*', end='')
+#         j+=1
+#     print('')
+#     i+=1
+
+# for i in range(1,101):
+#     print(i, end=' ')
+
+# A=[70,60,55,95,90,80,80,85,100]
+# sum=0
+# avg=0
+# for i in A:
+#     sum+=i
+#
+# avg=sum/len(A);
+# print(avg)
---
+++ practice/20190718/if_else.py
@@ -0,0 +1,14 @@
+a=1
+b=2
+c=10
+max=0
+if a>b and a>c:
+    max=a
+elif b>a and b>c:
+    max=b
+else:
+    max=c
+
+print(max)
+
+
