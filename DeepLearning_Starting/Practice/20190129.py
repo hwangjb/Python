f30a64575cba1ba848ff6e00d572dac7e2c47b21
hwangjb <qlghd98@gmail.com>
2020-01-29 16:43:18 +0900

    make def softmax

--- DeepLearning_Starting/Practice/20190129.py
+++ DeepLearning_Starting/Practice/20190129.py
@@ -86,3 +86,33 @@ network = init_network()
 x = np.array([1.0, 0.5])
 y = forward(network, x)
 print(y)
+
+# %%
+
+# 소프트맥스 함수
+a = np.array([0.3, 2.9, 4.0])
+exp_a = np.exp(a)
+print(exp_a)
+
+sum_exp_a = np.sum(exp_a)
+print(sum_exp_a)
+y = exp_a / sum_exp_a
+print(y)
+
+# %%
+
+# 소프트맥스 함수로 구현!!!!
+
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+
+a = np.array([0.3, 2.9, 4.0])
+y = softmax(a)
+print(y)
+print(np.sum(y))
