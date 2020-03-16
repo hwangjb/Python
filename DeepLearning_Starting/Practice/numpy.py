84f0177670170a6f0bb477f47d7eb0dd8a42297c
hwangjb <qlghd98@gmail.com>
2020-03-16 13:10:35 +0900

    second time Numpy

--- DeepLearning_Starting/Practice/numpy.py
+++ DeepLearning_Starting/Practice/numpy.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+# %%
 A = np.array([ [1, 0], [0, 1] ])
 B = np.array([ [1, 1], [1, 1] ])
 
@@ -38,3 +38,43 @@ print(c)
 c = c.reshape(1,3)
 print(c.shape)
 print(c)
+
+
+# %%
+a = np.array([[1, 2, 3], [4, 5, 6]])
+b = np.array([[-1, -2], [-3, -4], [-5, -6]])
+
+c = np.dot(a, b) # 행렬의 곱
+
+print("A.shape ==", a.shape, "B.shape ==", b.shape)
+print("C.shape ==", c.shape)
+print(c)
+
+# %%
+# numpy broadcast : 크기가 다른 두 행렬간에도 사칙연산을 할수 있음(행렬곱에서는 적용 안됨!!!!)
+
+a = np.array([[1, 2],[3, 4]])
+b = 5
+c = np.array([4, 5])
+
+print(a+b)
+print(a+c)
+
+# %%
+
+a = np.array([[1, 2], [3, 4], [5, 6]])
+
+b = a.T
+print(a.shape, b.shape)
+print(a)
+print(b)
+
+# %%
+A = np.array([[10, 20, 30, 40],[50, 60, 70, 80]])
+
+it = np.nditer(A, flags=['multi_index'], op_flags=['readwrite'])
+
+while not it.finished:
+    idx = it.multi_index
+    print("current value =>", A[it.multi_index])
+    it.iternext()
