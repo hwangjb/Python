fc0c72fb80ca260e594e48a416baa6163cbb231c
hwangjb <qlghd98@gmail.com>
2020-03-17 16:21:59 +0900

    third numpy

--- DeepLearning_Starting/Practice/numpy.py
+++ DeepLearning_Starting/Practice/numpy.py
@@ -78,3 +78,55 @@ while not it.finished:
     idx = it.multi_index
     print("current value =>", A[it.multi_index])
     it.iternext()
+
+
+# %%
+# concatenate 함수 : 서로 다른 행렬을 합쳐줌
+a = np.array([[1, 2, 3], [4, 5, 6]])
+
+row_add = np.array([7, 8, 9]).reshape(1,3)
+b = np.concatenate((a, row_add), axis=0)
+print(b)
+
+colum_add = np.array([10, 11]).reshape(2, 1)
+c = np.concatenate((a, colum_add), axis=1)
+print(c)
+
+# %%
+loaded_data = np.loadtxt('./dataset/data-01.csv', delimiter=',', dtype=np.float32)
+print(loaded_data)
+
+
+x_data = loaded_data[:, 0:-1]
+print(x_data)
+
+y_data = loaded_data[:, [-1]]
+print(y_data)
+
+# %%
+random_number1 = np.random.rand(3)
+random_number2 = np.random.rand(1, 3)
+random_number3 = np.random.rand(3, 1)
+
+print(random_number1, random_number1.shape)
+print(random_number2, random_number2.shape)
+print(random_number3, random_number3.shape)
+
+# %%
+x = np.array([[2, 4, 6], [1, 2, 3], [0, 5,8]])
+
+print(np.max(x, axis=0))
+print(np.min(x, axis=0))
+print(np.max(x, axis=1))
+print(np.min(x, axis=1))
+print(np.argmax(x, axis=0))
+print(np.argmin(x, axis=0))
+print(np.argmax(x, axis=1))
+print(np.argmin (x, axis=1))
+
+a = np.ones([3,3])
+b = np.zeros([3,2])
+print(a.shape, a)
+print(b.shape, b)
+
+
