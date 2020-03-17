de8f20dbd2e81bc2c574faebeb1b11446f653ac1
hwangjb <qlghd98@gmail.com>
2020-03-17 16:23:27 +0900

    pratice plot

---
+++ DeepLearning_Starting/Practice/matplotlib.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# %%
+# Scatter plot
+
+x_data = np.random.rand(100)
+y_data = np.random.rand(100)
+
+plt.title("scatter plot")
+plt.grid()
+plt.scatter(x_data, y_data, color='b', marker='o')
+plt.show()
+
+# %%
+#line plot
+
+x_data = [x for x in range(-5, 5)]
+y_data = [y*y for y in range(-5, 5)]
+
+plt.title("line plot")
+plt.grid()
+plt.plot(x_data, y_data, color='b')
+plt.show()
+# %%
+x_data = [x for x in range(-3, 10)]
+y_data = [-8, -13, 0, 3, 6, -1, -5, -7, 1, 8, 7, 12, 13]
+
+plt.title("line plot")
+plt.grid()
+plt.plot(x_data, y_data, color='b')
+plt.show()
