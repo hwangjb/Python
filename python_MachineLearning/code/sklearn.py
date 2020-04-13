60d4074d256040606507a764653ea9673665e903
hwangjb <qlghd98@gmail.com>
2020-04-13 20:29:26 +0900

    20200413 Ch 02 04 end

--- python_MachineLearning/code/sklearn.py
+++ python_MachineLearning/code/sklearn.py
@@ -6,7 +6,8 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import KFold
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold, cross_val_score, cross_validate
+from sklearn.model_selection import GridSearchCV
 
 # %%
 print(sklearn.__version__)
@@ -151,3 +152,39 @@ for train_index, test_index in skf.split(features, label):
 
 print('## 교차 검증별 정확도 :', np.round(cv_accuracy, 4))
 print('## 평균 검증 정확도 : ', np.mean(cv_accuracy))
+
+# %%
+iris_data = load_iris()
+dt_clf = DecisionTreeClassifier(random_state=156)
+
+data = iris_data.data
+label = iris_data.target
+
+scores = cross_val_score(dt_clf, data, label, scoring='accuracy', cv=3)
+
+print('교차 검증별 정확도 :', np.round(scores, 4))
+print('평균 검증 정확도 :', np.round(np.mean(scores), 4))
+
+# %%
+iris = load_iris()
+
+x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=121)
+
+dtree = DecisionTreeClassifier()
+
+parameters = {'max_depth': [1, 2, 3], 'min_samples_split': [2, 3]}
+
+grid_dtree = GridSearchCV(dtree, param_grid=parameters, cv=3, refit=True)
+
+grid_dtree.fit(x_train, y_train)
+
+scores_df = pd.DataFrame(grid_dtree.cv_results_)
+scores_df[['params', 'mean_test_score', 'rank_test_score', 'split0_test_score', 'split1_test_score', 'split2_test_score']]
+
+print('GridSearchCV 최적 파라미터 :', grid_dtree.best_params_)
+print('GridSearchCV 최고 정확도:{0:.4f}'.format(grid_dtree.best_score_))
+
+estimator = grid_dtree.best_estimator_
+
+pred = estimator.predict(x_test)
+print('%.4f' % accuracy_score(y_test, pred))
