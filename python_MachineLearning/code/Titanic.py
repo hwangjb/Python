47e6512488312497142cf73336a1139710c11061
hwangjb <qlghd98@gmail.com>
2020-04-14 17:37:57 +0900

    Titanic end

---
+++ python_MachineLearning/code/Titanic.py
@@ -0,0 +1,208 @@
+# %%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import preprocessing
+
+titanic_df = pd.read_csv('./python_MachineLearning/dataset/titanic/titanic_train.csv')
+# %%
+print(titanic_df.info())
+
+# %%
+titanic_df['Age'].fillna(titanic_df['Age'].mean(), inplace=True)
+titanic_df['Cabin'].fillna('N', inplace=True)
+titanic_df['Embarked'].fillna('N', inplace=True)
+print('data set Null Count :', titanic_df.isnull().sum().sum())
+
+# %%
+print(titanic_df['Sex'].value_counts())
+print(titanic_df['Cabin'].value_counts())
+print(titanic_df['Embarked'].value_counts())
+
+titanic_df['Cabin'] = titanic_df['Cabin'].str[:1]
+print(titanic_df['Cabin'].head(3))
+# %%
+print(titanic_df.groupby(['Sex', 'Survived'])['Survived'].count())
+
+sns.barplot(x='Sex', y='Survived', data=titanic_df)
+plt.show()
+
+# %%
+sns.barplot(x='Pclass', y='Survived', hue='Sex', data=titanic_df)
+plt.show()
+
+
+# %%
+def get_category(age):
+    cat = ''
+    if age <= -1:
+        cat = 'Unknown'
+    elif age <= 5:
+        cat = 'Baby'
+    elif age <= 12:
+        cat = 'Child'
+    elif age <= 18:
+        cat = 'Teenager'
+    elif age <= 25:
+        cat = 'Student'
+    elif age <= 35:
+        cat = 'Young Adult'
+    elif age <= 60:
+        cat = 'Adult'
+    else:
+        cat = 'Elderly'
+
+    return cat
+
+
+plt.figure(figsize=(10, 6))
+group_names = ['Unknown', 'Baby', 'Child', 'Teenager', 'Young Adult', 'Adult', 'Elderly']
+
+titanic_df['Age_cat'] = titanic_df['Age'].apply(lambda x: get_category(x))
+sns.barplot(x='Age_cat', y='Survived', hue='Sex', data=titanic_df, order=group_names)
+plt.show()
+titanic_df.drop('Age_cat', axis=1, inplace=True)
+
+
+# %%
+
+
+def encode_features(dataDF):
+    features = ['Cabin', 'Sex', 'Embarked']
+    for features in features:
+        le = preprocessing.LabelEncoder()
+        le = le.fit(dataDF[features])
+        dataDF[features] = le.transform(dataDF[features])
+
+    return dataDF
+
+
+titanic_df = encode_features(titanic_df)
+print(titanic_df.head())
+
+
+# %%
+# NULL 처리 함수
+
+def fillna(df):
+    df['Age'].fillna(df['Age'].mean(), inplace=True)
+    df['Cabin'].fillna('N', inplace=True)
+    df['Embarked'].fillna('N', inplace=True)
+    df['Fare'].fillna(0, inplace=True)
+    return df
+
+
+# 머신러닝 알고리즘에 불필요한 속성 제거
+
+def drop_features(df):
+    df.drop(['PassengerId', 'Name', 'Ticket'], axis=1, inplace=True)
+    return df
+
+
+# 레이블 인코딩 수행
+
+def format_features(df):
+    df['Cabin'] = df['Cabin'].str[:1]
+    feature = ['Cabin', 'Sex', 'Embarked']
+    for feature in feature:
+        le = preprocessing.LabelEncoder()
+        le = le.fit(df[feature])
+        df[feature] = le.transform(df[feature])
+
+    return df
+
+
+# 앞에서 설정한 데이터 전처리 함수 호출
+
+def transform_feature(df):
+    df = fillna(df)
+    df = drop_features(df)
+    df = format_features(df)
+
+    return df
+
+
+# %%
+y_titanic_df = titanic_df['Survived']
+x_titanic_df = titanic_df.drop('Survived', axis=1)
+x_titanic_df = transform_feature(x_titanic_df)
+
+# %%
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x_titanic_df, y_titanic_df, test_size=0.2, random_state=11)
+
+# %%
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+dt_clf = DecisionTreeClassifier(random_state=11)
+rf_clf = RandomForestClassifier(random_state=11)
+lr_clf = LogisticRegression()
+
+dt_clf.fit(x_train, y_train)
+dt_pred = dt_clf.predict(x_test)
+print('DecisionTreeClassifier 정확도 : {0:.4f}'.format(accuracy_score(y_test, dt_pred)))
+
+rf_clf.fit(x_train, y_train)
+rf_pred = rf_clf.predict(x_test)
+print('RandomForestClassifier 정확도 : {0:.4f}'.format(accuracy_score(y_test, rf_pred)))
+
+lr_clf.fit(x_train, y_train)
+lr_pred = lr_clf.predict(x_test)
+print('LogisticRegression 정확도 : {0:.4f}'.format(accuracy_score(y_test, lr_pred)))
+
+# %%
+from sklearn.model_selection import KFold
+
+
+def exec_kfold(clf, folds=5):
+    kfold = KFold(n_splits=folds)
+    scores = []
+
+    for iter_count, (train_index, test_index) in enumerate(kfold.split(x_titanic_df)):
+        x_train, x_test = x_titanic_df.values[train_index], x_titanic_df.values[test_index]
+        y_train, y_test = y_titanic_df.values[train_index], y_titanic_df[test_index]
+
+        clf.fit(x_train, y_train)
+        predictions = clf.predict(x_test)
+        accuracy = accuracy_score(y_test, predictions)
+        scores.append(accuracy)
+        print('교차 검증 {0} 정확도 : {1:.4f}'.format(iter_count, accuracy))
+
+    mean_score = np.mean(scores)
+    print('평균 정확도 : {0:.4f}'.format(mean_score))
+
+
+exec_kfold(dt_clf, folds=5)
+
+# %%
+
+from sklearn.model_selection import cross_val_score
+
+scores = cross_val_score(dt_clf, x_titanic_df, y_titanic_df, cv=5)
+
+for iter_count, accuracy in enumerate(scores):
+    print('교차 검증 {0} 정확도 : {1:.4f}'.format(iter_count, accuracy))
+
+print('평균 정확도 : {0:.4f}'.format(np.mean(scores)))
+
+# %%
+from sklearn.model_selection import GridSearchCV
+
+parameters = {'max_depth': [2, 3, 4, 10],
+              'min_samples_split': [2, 3, 4], 'min_samples_leaf': [1, 5, 8]}
+
+grid_dclf = GridSearchCV(dt_clf, param_grid=parameters, scoring='accuracy', cv=5)
+grid_dclf.fit(x_train, y_train)
+
+print('GridSearchCV 최적 하이퍼 파라미터 :', grid_dclf.best_params_)
+print('GridSearchCB 최적 정확도 : {0:.4f}'.format(grid_dclf.best_score_))
+best_dclf = grid_dclf.best_estimator_
+
+dpredictions = best_dclf.predict(x_test)
+accuracy = accuracy_score(y_test, dpredictions)
+print('테스트 세트에서의 DecisionTreeClassifier 정확도 : {0:.4f}'.format(accuracy))
